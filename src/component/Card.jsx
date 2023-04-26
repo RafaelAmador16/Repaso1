64ff491a8152e661d1d985a8dca72ca5731c1e7a
RafaelAmador16 <116183983+RafaelAmador16@users.noreply.github.com>
2023-04-26 17:21:41 +0200

    Arreglo de errores

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Card = ({personaje,index}) => {
 const urlParts = personaje.url.split('/');
-const id = urlParts.pop();
+const id = urlParts[urlParts.length-2];
   return (
     <div>
       <img
-        src={`https://starwars-visualguide.com/assets/img/characters/${urlParts.pop()}.jpg`}
+        src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
         width="30%"
         alt={personaje.name}
         
